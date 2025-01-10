c052013ae3d091ce3b02158b2e777098db8ea6b3
MitjaCH <mitja.kurath@mitja.io>
2025-01-10 09:43:51 +0100

    Add utility functions for delay and saving coupons as JSON file

---
+++ src/utils.rs
@@ -0,0 +1,15 @@
+use crate::structs::StoreCoupons;
+use serde_json::to_writer_pretty;
+use std::fs::File;
+use std::io::Write;
+use std::time::Duration;
+use tokio::time::sleep;
+
+pub async fn delay(ms: u64) {
+    sleep(Duration::from_millis(ms)).await;
+}
+
+pub async fn save_coupons(file_path: &str, coupons: &[StoreCoupons]) {
+    let file = File::create(file_path).unwrap();
+    to_writer_pretty(file, &coupons).unwrap();
+}
